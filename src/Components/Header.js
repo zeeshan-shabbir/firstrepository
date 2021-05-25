1fa57bdd8ba8461aa596801dafcb5baf6183ae39
zeeshan <zeeshanshabbir186@gmail.com>
2021-05-25 20:57:29 +0500

    error version

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
+    const isActive = {
+        fontWeight: "bold",
+        backgroundColor: "rgba(255, 255, 255, 0.1)",
+    };
+    const [header, setHeader] = useState("header")
+
+    const listenScrollEvent = (event) => {
+        if (window.scrollY < 73) {
+            return setHeader("header")
+        } else if (window.scrollY > 70) {
+            return setHeader("header2")
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', listenScrollEvent);
+
+        return () =>
+            window.removeEventListener('scroll', listenScrollEvent);
+    }, []);
     return (
         <div>
             <div className="nav_wrapper">
-                <div className="container-100 bg-white shadow fixed">
-                    <div className='container  flex justify-between align-center '>
+                <div className="container bg-white shadow fixed">
+                    <div className={header}>
                         <div className='Logo'>
                             <NavLink exact to="/" className='nav-Link'>  <p>My Logo</p></NavLink>
                             {/* <img src={logo} className="logo" alt="logo" /> */}
@@ -15,22 +35,24 @@ export default function Header() {
                         <nav>
                             <ul className='flexallcenter'>
                                 <li className="nav-item">
-                                    <NavLink exact to="/" className='nav-Link'>Home</NavLink>
+                                    <NavLink exact to="/" activeClassName="selected" className='nav-Link'
+                                        activeStyle={isActive}
+                                    >Home</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink to="/About" className='nav-Link'>About</NavLink>
+                                    <NavLink exact to="/About" className='nav-Link' activeStyle={isActive}>About</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink exact to="/Services" className='nav-Link'>Services</NavLink>
+                                    <NavLink exact to="/Services" className='nav-Link' activeStyle={isActive}>Services</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink exact to="/" className='nav-Link'>Testimonials</NavLink>
+                                    <NavLink exact to="/Testimonials" className='nav-Link' activeStyle={isActive}>Testimonials</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink exact to="/My_work" className='nav-Link'>My Work</NavLink>
+                                    <NavLink exact to="/My_work" className='nav-Link' activeStyle={isActive}>My Work</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink exact to="/" className='nav-Link'>Contact</NavLink>
+                                    <NavLink exact to="/Contact" className='nav-Link' activeStyle={isActive}>Contact</NavLink>
                                 </li>
                             </ul>
                         </nav>
